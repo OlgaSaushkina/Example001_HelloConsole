30fac249736ef55d0285fa5998ff0bbf2eccc06f
OlgaSaushkina <soljuwka@gmail.com>
2022-08-20 14:23:19 +0700

    add Example 007 Magic

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите ваше имя: ");
-string username=Console.ReadLine();
-if(username.ToLower() == "маша" )
+string username = Console.ReadLine();
+if (username.ToLower() == "маша" )
 {
     Console.WriteLine ("Ура, это же Маша!");
 }
