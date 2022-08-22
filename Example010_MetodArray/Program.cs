1cd551b0e00e3d815ce50e78f7df7f46248ee171
OlgaSaushkina <soljuwka@gmail.com>
2022-08-22 10:23:58 +0700

    add Example 010 MetodArray

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array={1, 45, 5, 34, 5, 2, 45, 34, 33};
+int n = array.Length; 
+int index = 0;
+int find = 34;
+while (index < n )
+{
+    if (array[index] == find)
+   
+{
+     Console.WriteLine(index);
+     break;     
+}
+
+    index = index+1;
+}
+
