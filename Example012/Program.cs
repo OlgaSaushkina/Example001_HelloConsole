a1317e4420c029b0352771221321ff99a1230a71
OlgaSaushkina <soljuwka@gmail.com>
2022-08-27 17:41:26 +0700

    add EExample 012

---
+++ Example012/Program.cs
@@ -0,0 +1,75 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = “qwerty”
+// 012345
+/* string Replase(string text, char oldValue, char newValue)
+{
+string result = String.Empty;
+int length = text.Length;
+for (int i = 0; i < length; i++)
+{
+if(text[i] == oldValue) result=result + $"{newValue}";
+else result=result + $"{text[i]}";
+}
+return result;
+}
+string newText = Replase(text,' ','|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(text, 'к', 'К');
+Console.WriteLine(newText); */
+
+
+
+/* int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write($"{array[i]}");
+}
+Console.WriteLine();
+}
+PrintArray(arr); */
+
+
+
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write($"{array[i]}");
+}
+Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+int minPosition = i;
+for (int j = i+1; j < array.Length; j++)
+{
+if(array[j] < array[minPosition]) minPosition = j;
+{
+minPosition = j;
+}
+} int temporary =array[i];
+array[i] = array[minPosition];
+array[minPosition] = temporary;
+}
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
